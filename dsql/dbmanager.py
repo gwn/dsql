5b8cafa4b265cbf44f0cfdd51e59b7c5157ad76f
Ege Avunc <egeavunc@gmail.com>
2016-12-24 08:53:21 +0000

    Add support for the "in" operator

--- dsql/dbmanager.py
+++ dsql/dbmanager.py
@@ -312,7 +312,10 @@ def make(dbconn, dialect='standard'):
             else:
                 field, op = predicate, '='
 
-            placeholders = '%s'
+            if op == 'in' and isinstance(value, list):
+                placeholders = '(' + ','.join(['%s'] * len(value)) + ')'
+            else:
+                placeholders = '%s'
 
             return '%s %s %s' % (quote_identifier(field),
                                  validate_operator(op),
@@ -322,16 +325,27 @@ def make(dbconn, dialect='standard'):
             return ' AND '.join(build_single_comparation(predicate, value)
                                    for predicate, value in cond.items())
 
+        def flatten(inlist):
+            outlist = []
+
+            for item in inlist:
+                if isinstance(item, list):
+                    outlist.extend(item)
+                else:
+                    outlist.append(item)
+
+            return outlist
+
         tpl = ' OR '.join(['(%s)' % build_comparation_group(cond) for cond in conditions])
 
-        values = list(chain(*(cond.values() for cond in conditions)))
+        values = list(chain(*(flatten(cond.values()) for cond in conditions)))
 
         return '%s %s' % (keyword, tpl), values
 
 
     def validate_operator(op):
         supported_operators = (
-            '=', '>', '<', '!=', '<=', '>=',
+            '=', '>', '<', '!=', '<=', '>=', 'in',
         )
 
         if op not in supported_operators:
